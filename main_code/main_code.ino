af290fc1fb0fd2244230546fb501cd09d416e3dc
ELDtotheL <33078663+ELDtotheL@users.noreply.github.com>
2017-10-24 22:36:02 -0500

    Update main_code.ino

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,6 +1,7 @@
 //  My First Rocket
 //  Do not use delay!!! There might be other timing critical routines
-//  To add your function, use a .h and a .cpp file  
+//  To add your function, use a .h and a .cpp file 
+//  Just testing to see if I can add to this
 
 void setup() {
   // put your setup code here, to run once:
